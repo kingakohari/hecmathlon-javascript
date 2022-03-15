2004c32b723e84dd3ec51a0971382e75124a3b25
Kinga Kohari <kinguca@yahoo.com>
2022-03-15 01:20:54 +0100

    Issues with MIME type

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,63 @@
-import { HECMAthlon } from "main.js"
+class HECMAthlon {
+
+    /*  getMaxValue: function (input) {
+        const maxValues = input.split(",");
+        let maxValue = 0;
+        for(let i = 0; i < maxValues.length; i++){
+            const tempValue = parseInt(maxValues[i]);
+            if (tempValue > maxValue) {
+                maxValue = tempValue;
+            }
+        }
+        return [maxValue];
+    }, */
+
+    getMaxValue = (input) => {
+   
+        return input.reduce((a, b) => Math.max(a, b));   
+    }; 
+
+/*  getGreaterThan: function (input) {
+        const greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const greaterNums = [];
+        for(let j = 0; j < greaterValues.length; j++) {
+            if (greaterValues[j] > parseInt(input)) {
+                greaterNums.push(greaterValues[j]);
+            }
+        }
+        return greaterNums;
+    }, */
+
+    getGreaterThan = (input) => {
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter( n => n > parseInt(input));
+    };
+
+
+/*  fizzBuzz: function (input) {
+        const output = [];
+        for (let k = 1; k <= parseInt(input); k++) {
+            let value;
+            if(k % 5 === 0 && k % 3 === 0) value = "FizzBuzz";
+            else if(k % 3 === 0) value = "Fizz";
+            else if(k % 5 === 0) value = "Buzz";
+            else value = k;
+            output.push(value);
+        }
+        return output; */
+
+
+    fizzBuzz =  () => {
+        return [...Array(100).keys()].map((_, k) => k + 1)
+        .map(k => (k % 3 === 0 && k % 5 === 0)
+                  ? "FizzBuzz"
+                  : (k % 3 === 0)
+                  ? "Fizz"
+                  : (k % 5 === 0)
+                  ? "Buzz"
+                  : k)
+        .join("\n")
+    }       
+} 
 
 const hECMAthlonRefactor = new HECMAthlon();
 
@@ -29,6 +88,7 @@ function init() {
     });
 }
 
+
 function getOutput(output) {
 
   /* const returnValue = ["The function starts"]; */
@@ -53,5 +113,4 @@ function logResult(place, values){
         place.insertAdjacentHTML("beforeend", "<div>" + values[j] + "</div>");
     }
 }
-
 
