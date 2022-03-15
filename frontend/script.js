4d602b7aad484bd3798e762c82f81cc06bf5e7f2
Kinga Kohari <kinguca@yahoo.com>
2022-03-15 11:09:10 +0100

    Running tests

--- frontend/script.js
+++ frontend/script.js
@@ -12,7 +12,7 @@ class HECMAthlon {
         return [maxValue];
     }, */
 
-    getMaxValue = (input) => {
+    getMaxValue(input) {
    
         return input.reduce((a, b) => Math.max(a, b));   
     }; 
@@ -28,7 +28,7 @@ class HECMAthlon {
         return greaterNums;
     }, */
 
-    getGreaterThan = (input) => {
+    getGreaterThan(input) {
         return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter( n => n > parseInt(input));
     };
 
@@ -46,7 +46,7 @@ class HECMAthlon {
         return output; */
 
 
-    fizzBuzz =  () => {
+    fizzBuzz() {
         return [...Array(100).keys()].map((_, k) => k + 1)
         .map(k => (k % 3 === 0 && k % 5 === 0)
                   ? "FizzBuzz"
@@ -61,7 +61,7 @@ class HECMAthlon {
 
 const hECMAthlonRefactor = new HECMAthlon();
 
-function init() {
+const init = _ => {
     const maxValueBtn = document.querySelector(".maxValue__btn");
     const greaterThanBtn = document.querySelector(".greaterThan__btn");
     const fizzBuzzBtn = document.querySelector(".fizzBuzz__btn");
@@ -74,38 +74,34 @@ function init() {
     const greaterThanRoot = document.querySelector(".greaterThan__container");
     const fizzBuzzRoot = document.querySelector(".fizzBuzz__container");
 
-    maxValueBtn.addEventListener('click', () => {
+    maxValueBtn.addEventListener("click", () => {
         let maxValue = getOutput(hECMAthlonRefactor.getMaxValue(maxValueInput.value));
         logResult(maxValueRoot, maxValue);
     });
-    greaterThanBtn.addEventListener('click', () => {
+    greaterThanBtn.addEventListener("click", () => {
         let greaterValues = getOutput(hECMAthlonRefactor.getGreaterThan(greaterThanInput.value));
         logResult(greaterThanRoot, greaterValues);
     });
-    fizzBuzzBtn.addEventListener('click', () => {
+    fizzBuzzBtn.addEventListener("click", () => {
         let result = getOutput(hECMAthlonRefactor.fizzBuzz(fizzBuzzInput.value));
         logResult(fizzBuzzRoot, result);
     });
 }
 
 
-function getOutput(output) {
+const getOutput = output => ["The function starts", ...output, "The function ends"]
 
-  /* const returnValue = ["The function starts"]; */
+  /* const returnValue = ["The function starts"]; 
 
     for(let i = 0; i < output.length; i++){
 
-        /* returnValue.push(output[i]); */
-
-        return ["The function starts", ...output, "The function ends"]
-    }
-}
-/*         
+         returnValue.push(output[i]); 
+         
    returnValue.push("The function ends");
     return returnValue;
 } */
 
-function logResult(place, values){
+const logResult = (place, values) => {
     while(place.firstChild) {
         place.firstChild.remove();
     }
@@ -114,3 +110,4 @@ function logResult(place, values){
     }
 }
 
+init();
